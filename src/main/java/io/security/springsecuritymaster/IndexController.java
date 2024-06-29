7cd695d3947a7d782dccda93b0865d59005c4e12
choi <younggeunn@naver.com>
2024-06-29 11:10:02 +0900

    폼 인증 - formLogin()

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -11,4 +11,14 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
 }
