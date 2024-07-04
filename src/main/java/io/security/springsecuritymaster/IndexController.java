3b6e7a26612230ebd6c095b51408be8bae334ef0
choi <younggeunn@naver.com>
2024-07-04 13:41:22 +0900

    [인프런] 스프링 시큐리티 완전 정복

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -11,4 +11,19 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/denied")
+    public String denied(){
+        return "denied";
+    }
+
+    @GetMapping("/login")
+    public String customLogin(){
+        return "loginPage 를 구현해야 한다";
+    }
+
 }
