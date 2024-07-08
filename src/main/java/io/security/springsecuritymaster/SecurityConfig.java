3eadd08d6e51a86f97ce38f70480c3764f293432
choi <younggeunn@naver.com>
2024-07-08 20:39:54 +0900

    [인프런] 스프링 시큐리티 완전 정복

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -2,44 +2,82 @@ package io.security.springsecuritymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcherEntry;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    /*
+        - RequestMatcherDelegatingAuthorizationManager
+          • RequestMatcherDelegatingAuthorizationManager 의 mappings 속성에 직접 RequestMatcherEntry 객체를 생성하고 추가한다
+          • RequestMatcherDelegatingAuthorizationManager 를 감싸는 CustomRequestMatcherDelegatingAuthorizationManager 를 구현하여
+            모든 요청에 대한 권한 검사를 CustomRequestMatcherDelegatingAuthorizationManager 가 처리하도록 설정할 수 있다.
+          • RequestMatcherDelegatingAuthorizationManager > CustomRequestMatcherDelegatingAuthorizationManager > RequestMatcherDelegatingAuthorizationManager 구조로 위임이 되기 때문에
+            좋지 않은 방법이다. (구조가 개선된 방식은 실전프로젝트에서 진행예정.)
+     */
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+        http
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().access(authorizationManager(null)))
+                .formLogin(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() { // 해당 설정은 yml에서도 가능. 우선순위는 자바 설정이 더 높음.
-        UserDetails user = User.withUsername("user")
-                .password("{noop}1111")
-                .roles("USER")
-                .build();
+    public AuthorizationManager<RequestAuthorizationContext> authorizationManager(HandlerMappingIntrospector introspector) {
+        List<RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>>> mappings = new ArrayList<>();
 
-        UserDetails user2 = User.withUsername("user2")
-                .password("{noop}2222")
-                .roles("USER")
-                .build();
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry1 =
+                new RequestMatcherEntry<>(new MvcRequestMatcher(introspector, "/user"),
+                        AuthorityAuthorizationManager.hasAuthority("ROLE_USER"));
 
-        UserDetails user3 = User.withUsername("user3")
-                .password("{noop}3333")
-                .roles("USER")
-                .build();
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry2 =
+                new RequestMatcherEntry<>(new MvcRequestMatcher(introspector, "/db"),
+                        AuthorityAuthorizationManager.hasAuthority("ROLE_DB"));
 
-        return new InMemoryUserDetailsManager(user, user2, user3);
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry3 =
+                new RequestMatcherEntry<>(new MvcRequestMatcher(introspector, "/admin"),
+                        AuthorityAuthorizationManager.hasRole("ADMIN"));
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry4 =
+                new RequestMatcherEntry<>(AnyRequestMatcher.INSTANCE, new AuthenticatedAuthorizationManager<>());
+
+        mappings.add(requestMatcherEntry1);
+        mappings.add(requestMatcherEntry2);
+        mappings.add(requestMatcherEntry3);
+        mappings.add(requestMatcherEntry4);
+
+        return new CustomRequestMatcherDelegatingAuthorizationManager(mappings);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+        UserDetails db = User.withUsername("db").password("{noop}1111").roles("DB").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}1111").roles("ADMIN", "SECURE").build();
+        return new InMemoryUserDetailsManager(user, db, admin);
     }
 
 }
