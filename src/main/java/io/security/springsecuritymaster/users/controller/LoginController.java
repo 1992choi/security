15fd20fd1a2f7da932a8d3e473cd0109ee236995
choi <younggeunn@naver.com>
2024-07-12 18:24:10 +0900

    [실전 프로젝트] 커스텀 AuthenticationProvider 구현하기

--- src/main/java/io/security/springsecuritymaster/users/controller/LoginController.java
+++ src/main/java/io/security/springsecuritymaster/users/controller/LoginController.java
@@ -1,5 +1,10 @@
 package io.security.springsecuritymaster.users.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,6 +19,17 @@ public class LoginController {
     @GetMapping(value = "/signup")
     public String signup() {
         return "login/signup";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if (authentication != null) {
+            // 로그아웃 관련 핸들러는 많으나 세션을 무효화시키는 핸들러 선택
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/login";
     }
 
 }
