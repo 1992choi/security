7b8220e9affe49c83c52e256828b584efc21197a
choi <younggeunn@naver.com>
2024-07-12 17:39:43 +0900

    [실전 프로젝트] 커스텀 AuthenticationProvider 구현하기

---
+++ src/main/java/io/security/springsecuritymaster/security/configs/AuthConfig.java
@@ -0,0 +1,46 @@
+package io.security.springsecuritymaster.security.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AuthConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        /*
+            - 기본적으로 bcrypt 암호화 알고리즘의 BCryptPasswordEncoder 객체를 생성하고 사용.
+
+            - 아래처럼 알고리즘을 지정할 수도 있다.
+                Ex)
+                String encodingId = "pbkdf2";
+                Map<String, PasswordEncoder> encoders = new HashMap<>();
+                encoders.put(encodingId, Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+                DelegatingPasswordEncoder delegatingPasswordEncoder = new DelegatingPasswordEncoder(encodingId, encoders);
+                return delegatingPasswordEncoder;
+
+            - 시큐리티 지원 암호화 알고리즘 유형
+                String encodingId = "bcrypt";
+                Map<String, PasswordEncoder> encoders = new HashMap<>();
+                encoders.put(encodingId, new BCryptPasswordEncoder());
+                encoders.put("ldap", new org.springframework.security.crypto.password.LdapShaPasswordEncoder());
+                encoders.put("MD4", new org.springframework.security.crypto.password.Md4PasswordEncoder());
+                encoders.put("MD5", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("MD5"));
+                encoders.put("noop", org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance());
+                encoders.put("pbkdf2", Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_5());
+                encoders.put("pbkdf2@SpringSecurity_v5_8", Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+                encoders.put("scrypt", SCryptPasswordEncoder.defaultsForSpringSecurity_v4_1());
+                encoders.put("scrypt@SpringSecurity_v5_8", SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8());
+                encoders.put("SHA-1", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-1"));
+                encoders.put("SHA-256",
+                new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-256"));
+                encoders.put("sha256", new org.springframework.security.crypto.password.StandardPasswordEncoder());
+                encoders.put("argon2", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_2());
+                encoders.put("argon2@SpringSecurity_v5_8", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+         */
+    }
+
+}
--- src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
@@ -3,14 +3,9 @@ package io.security.springsecuritymaster.security.configs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -18,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final UserDetailsService userDetailsService;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,44 +23,9 @@ public class SecurityConfig {
                         .requestMatchers("/", "/signup").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/login").permitAll())
-                .userDetailsService(userDetailsService);
+                .authenticationProvider(authenticationProvider);
 
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        /*
-            - 기본적으로 bcrypt 암호화 알고리즘의 BCryptPasswordEncoder 객체를 생성하고 사용.
-
-            - 아래처럼 알고리즘을 지정할 수도 있다.
-                Ex)
-                String encodingId = "pbkdf2";
-                Map<String, PasswordEncoder> encoders = new HashMap<>();
-                encoders.put(encodingId, Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8());
-                DelegatingPasswordEncoder delegatingPasswordEncoder = new DelegatingPasswordEncoder(encodingId, encoders);
-                return delegatingPasswordEncoder;
-
-            - 시큐리티 지원 암호화 알고리즘 유형
-                String encodingId = "bcrypt";
-                Map<String, PasswordEncoder> encoders = new HashMap<>();
-                encoders.put(encodingId, new BCryptPasswordEncoder());
-                encoders.put("ldap", new org.springframework.security.crypto.password.LdapShaPasswordEncoder());
-                encoders.put("MD4", new org.springframework.security.crypto.password.Md4PasswordEncoder());
-                encoders.put("MD5", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("MD5"));
-                encoders.put("noop", org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance());
-                encoders.put("pbkdf2", Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_5());
-                encoders.put("pbkdf2@SpringSecurity_v5_8", Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8());
-                encoders.put("scrypt", SCryptPasswordEncoder.defaultsForSpringSecurity_v4_1());
-                encoders.put("scrypt@SpringSecurity_v5_8", SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8());
-                encoders.put("SHA-1", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-1"));
-                encoders.put("SHA-256",
-                new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-256"));
-                encoders.put("sha256", new org.springframework.security.crypto.password.StandardPasswordEncoder());
-                encoders.put("argon2", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_2());
-                encoders.put("argon2@SpringSecurity_v5_8", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8());
-         */
     }
 
 }
