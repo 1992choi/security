35c4712e52848fd1e10499aaea9bd9e881f9cca3
choi <younggeunn@naver.com>
2024-07-12 16:28:28 +0900

    [실전 프로젝트] 회원가입 & PasswordEncoder

--- README.md
+++ README.md
@@ -169,3 +169,16 @@
 
 ### basic-48-redis
 - Redis 를 활용한 이중화 설정 - @EnableRedisHttpSession
+
+<br><br>
+
+## 기타 설정
+### PostgreSQL
+- 설치 및 실행
+  - docker pull postgres
+  - docker run --name postgres -v ~/postgres-workspace/postgresdb:/var/lib/postgresql/data -e POSTGRES_PASSWORD=0000 -p 5432:5432 -d postgres
+- 접속
+  - docker exec -it [CONTAINER ID] bash
+  - psql -U [username]
+- DB 생성
+  - create database [name];
--- src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -19,11 +21,46 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
-                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/", "/signup").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/login").permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        /*
+            - 기본적으로 bcrypt 암호화 알고리즘의 BCryptPasswordEncoder 객체를 생성하고 사용.
+
+            - 아래처럼 알고리즘을 지정할 수도 있다.
+                Ex)
+                String encodingId = "pbkdf2";
+                Map<String, PasswordEncoder> encoders = new HashMap<>();
+                encoders.put(encodingId, Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+                DelegatingPasswordEncoder delegatingPasswordEncoder = new DelegatingPasswordEncoder(encodingId, encoders);
+                return delegatingPasswordEncoder;
+
+            - 시큐리티 지원 암호화 알고리즘 유형
+                String encodingId = "bcrypt";
+                Map<String, PasswordEncoder> encoders = new HashMap<>();
+                encoders.put(encodingId, new BCryptPasswordEncoder());
+                encoders.put("ldap", new org.springframework.security.crypto.password.LdapShaPasswordEncoder());
+                encoders.put("MD4", new org.springframework.security.crypto.password.Md4PasswordEncoder());
+                encoders.put("MD5", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("MD5"));
+                encoders.put("noop", org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance());
+                encoders.put("pbkdf2", Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_5());
+                encoders.put("pbkdf2@SpringSecurity_v5_8", Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+                encoders.put("scrypt", SCryptPasswordEncoder.defaultsForSpringSecurity_v4_1());
+                encoders.put("scrypt@SpringSecurity_v5_8", SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8());
+                encoders.put("SHA-1", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-1"));
+                encoders.put("SHA-256",
+                new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-256"));
+                encoders.put("sha256", new org.springframework.security.crypto.password.StandardPasswordEncoder());
+                encoders.put("argon2", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_2());
+                encoders.put("argon2@SpringSecurity_v5_8", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+         */
     }
 
     @Bean
